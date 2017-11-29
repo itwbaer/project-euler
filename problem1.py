651af19e914b06ccf056ec1877553985989b6915
Ian Baer <itwbaer@gmail.com>
2017-11-28 22:18:39 -0600

    Problems 1 and 2 completed, 3 started

--- problem1.py
+++ problem1.py
@@ -1,4 +1,6 @@
-"""If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+""" Multiples of 3 and 5
+    Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
@@ -7,20 +9,20 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 def is_divisible_by(n, d):
     # if the remainder is equal to 0, then n is divisible by d
-    print(True if n % d == 0 else False)
+
     return True if n % d == 0 else False
 
 
 def sum_of_multiples(lowerLimit, upperLimit, divisors):
     multSum = 0
 
-    for n in range(lowerLimit, upperLimit+1):
+    for n in range(lowerLimit, upperLimit):
         #for each divisor, test if n divisible by it
         for d in divisors:
             if is_divisible_by(n, d):
                 multSum = (multSum + n)
                 break
-        print(n)
+
     return multSum
 
 
---
+++ problem3.py
@@ -0,0 +1,15 @@
+""" Largest prime factor
+    Problem 3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+
+def largest_prime_factor(n):
+
+    return 0
+
+
+print(largest_prime_factor(600851475143))
